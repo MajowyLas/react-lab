a3d9985341c0ad5173b2a5e82add139b7bf584af
MajowyLas <anna.maywald@agh.edu.pl>
2026-05-16 12:11:52 +0200

    Decomposed afterLoginState to UserPanel.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 import "milligram";
+import LoginForm from "./LoginForm";
+import UserPanel from "./UserPanel";
 
 function App() {
     //let email = 'aniamaywald@gmail.com';
-    const [email, setEmail] = useState('aniamaywald@gmail.com');
+    const [email, setEmail] = useState('');
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     function handleChange(event) {
@@ -21,6 +23,14 @@ function App() {
         setIsLoggedIn(false);
     }
 
+    function login(loginForm) {
+        setEmail(email);
+        setIsLoggedIn(true)
+    }
+    function logout() {
+        setIsLoggedIn('')
+    }
+
 
 
     let message;
@@ -34,28 +44,26 @@ function App() {
     }
 
 
-
     return (
         <div>
             <h1>Witaj w systemie do zapisów na zajęcia</h1>
 
-            {!isLoggedIn ? (
-
-                <div>
-                    <h2>{message}</h2>
-                    <label>
-                        Zaloguj się e-mailem
-                        <input type="text" value={email} onChange={handleChange} />
-                    </label>
-                    <button type="button" onClick={handleLogin}>Wchodzę</button>
-                </div>
+           <div>
+                <h2>{message}</h2>
+                <LoginForm onLogin={login}/>
+        </div>
             ) : (
 
-                <div>
-                    <h2>Witaj {email}!</h2>
-                    <a href="#" onClick={handleLogout}>Wyloguj</a>
-                </div>
-            )}
+            return (
+            <div>
+                <h1>System do zapisów na zajęcia</h1>
+                {
+                    isLoggedIn
+                        ? <UserPanel username={isLoggedIn} onLogout={logout}/>
+                        : <LoginForm onLogin={login}/>
+                }
+            </div>
+            );
         </div>
     );
 }
---
+++ src/UserPanel.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function UserPanel(props) {
+    return (
+        <div>
+            <h2>Witaj {props.username}!</h2>
+            <a href="#" onClick={props.onLogout}>Wyloguj</a>
+        </div>
+    );
+}
