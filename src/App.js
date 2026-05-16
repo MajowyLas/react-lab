fe9ba7f8010bf5a1f5db0bfa0d706fcd5107fc68
MajowyLas <anna.maywald@agh.edu.pl>
2026-05-16 11:03:47 +0200

    fix dla weryfikacji długosci maila - zmiana ilości znaków

--- src/App.js
+++ src/App.js
@@ -3,32 +3,49 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
-   // let email = 'aniamaywald@gmail.com';
-    const [email,setEmail] = useState('aniamaywald@gmail.com');
+    // let email = 'aniamaywald@gmail.com';
+    const [email, setEmail] = useState('aniamaywald@gmail.com');
+    const [isLogeedIn, setIsLoggedIn] = useState(false);
 
     function handleChange(event) {
         //console.log(event.target.value);
         setEmail(event.target.value);
     }
-        let message;
 
-        if (email.length < 10) {
-            message = <div>Ale masz krótki adres!</div>;
-        } else if (email.length < 15) {
-            message = <div>Twój adres e-mail jest w sam raz.</div>;
-        } else {
-            message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
-        }
+    function handelLogin() {
+        setIsLoggedIn(true);
+    }
+
+    function handleLogout() {
+        setIsLoggedIn(false);
+    }
+
+    let message;
+
+    if (email.length < 10) {
+        message = <div>Ale masz krótki adres!</div>;
+    } else if (email.length < 25) {
+        message = <div>Twój adres e-mail jest w sam raz.</div>;
+    } else {
+        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
+    }
 
 
-    return (
-        <div>
-            <h1> System do zapisów na zajęcia </h1>
-            <h2>  {message}     </h2>
-            <input type="text" value={email}
-                   onChange={handleChange}/>
+    if (!isLogeedIn) {
+
+        return (
+            <div>
+                <h1> System do zapisów na zajęcia </h1>
+                <h2>  {message}     </h2>
+                <input type="text" value={email}
+                       onChange={handleChange}/>
+                <button type="button" onClick={() => alert(email)}>
+                    Wyświetl mój e-mail w alercie
+                </button>
             </div>
-    );
+        );
+    }
+
 }
 
 export default App;
