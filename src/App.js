1647f7b2286140d2de7c95084b862e31af98128d
MajowyLas <anna.maywald@agh.edu.pl>
2026-05-16 11:36:43 +0200

    Wykorzystaj wszystkie poznane informacje i wykonaj stronę wejściową do naszej aplikacji.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
-    // let email = 'aniamaywald@gmail.com';
+    //let email = 'aniamaywald@gmail.com';
     const [email, setEmail] = useState('aniamaywald@gmail.com');
-    const [isLogeedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     function handleChange(event) {
         //console.log(event.target.value);
         setEmail(event.target.value);
     }
 
-    function handelLogin() {
+    function handleLogin() {
         setIsLoggedIn(true);
     }
 
     function handleLogout() {
         setIsLoggedIn(false);
     }
+
+
 
     let message;
 
@@ -32,39 +34,29 @@ function App() {
 
 
 
+    return (
+        <div>
+            <h1>Witaj w systemie do zapisów na zajęcia</h1>
 
-        return (
-            <div>
-                <h1> System do zapisów na zajęcia </h1>
-                <h2>  {message}     </h2>
-                <input type="text" value={email}
-                       onChange={handleChange}/>
-                <button type="button" onClick={() => alert(email)}>
-                    Wyświetl mój e-mail w alercie
-                </button>
-            </div>
-        );
+            {!isLoggedIn ? (
 
+                <div>
+                    <h2>{message}</h2>
+                    <label>
+                        Zaloguj się e-mailem
+                        <input type="text" value={email} onChange={handleChange} />
+                    </label>
+                    <button type="button" onClick={handleLogin}>Wchodzę</button>
+                </div>
+            ) : (
 
+                <div>
+                    <h2>Witaj {email}!</h2>
+                    <a href="#" onClick={handleLogout}>Wyloguj</a>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
-// “udostępnij z tego pliku następującą implementację komponentu”.
-
-
-//return (
-   // <div>
-      //  <h1> System do zapisów na zajęcia </h1>
-      //  <h2>
-        //    {
-           //     email.length === 0
-          //          ? 'Wpisz swój email'
-           //         : email.length < 10
-           //             ? 'Krótki e-mail'
-          //              : `Twój e-mail to ${email}.`
-         //   }
-      //  </h2>
-     //   <input type="text" value={email}
-      //         onChange={handleChange}/>
-   // </div>
-//);
