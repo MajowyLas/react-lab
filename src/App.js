85a747fdbb7cd3a453a74ec1ccd95341a0cb0984
MajowyLas <anna.maywald@agh.edu.pl>
2026-05-16 10:02:54 +0200

    Warunki logiczne w szablonie -> weryfikacja długosci emaila -> zmiana metody

--- src/App.js
+++ src/App.js
@@ -10,18 +10,20 @@ function App() {
         //console.log(event.target.value);
         setEmail(event.target.value);
     }
+        let message;
+
+        if (email.length < 10) {
+            message = <div>Ale masz krótki adres!</div>;
+        } else if (email.length < 15) {
+            message = <div>Twój adres e-mail jest w sam raz.</div>;
+        } else {
+            message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
+        }
+
     return (
         <div>
             <h1> System do zapisów na zajęcia </h1>
-            <h2>
-                {
-                    email.length === 0
-                        ? 'Wpisz swój email'
-                    : email.length <10
-                    ? 'Krótki e-mail'
-                    : `Twój e-mail to ${email}.`
-                }
-                </h2>
+            <h2>  {message}     </h2>
             <input type="text" value={email}
                    onChange={handleChange}/>
             </div>
@@ -30,3 +32,21 @@ function App() {
 
 export default App;
 // “udostępnij z tego pliku następującą implementację komponentu”.
+
+
+//return (
+   // <div>
+      //  <h1> System do zapisów na zajęcia </h1>
+      //  <h2>
+        //    {
+           //     email.length === 0
+          //          ? 'Wpisz swój email'
+           //         : email.length < 10
+           //             ? 'Krótki e-mail'
+          //              : `Twój e-mail to ${email}.`
+         //   }
+      //  </h2>
+     //   <input type="text" value={email}
+      //         onChange={handleChange}/>
+   // </div>
+//);
