2f9f264abbc465579126d119dcb9eba021a52d31
MajowyLas <anna.maywald@agh.edu.pl>
2026-05-16 09:57:14 +0200

    Warunki logiczne w szablonie -> weryfikacja długosci emaila

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'aniamaywald@gmail.com';
+   // let email = 'aniamaywald@gmail.com';
+    const [email,setEmail] = useState('aniamaywald@gmail.com');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        //console.log(event.target.value);
+        setEmail(event.target.value);
     }
     return (
         <div>
             <h1> System do zapisów na zajęcia </h1>
-            <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            <h2>
+                {
+                    email.length === 0
+                        ? 'Wpisz swój email'
+                    : email.length <10
+                    ? 'Krótki e-mail'
+                    : `Twój e-mail to ${email}.`
+                }
+                </h2>
+            <input type="text" value={email}
+                   onChange={handleChange}/>
             </div>
     );
 }
